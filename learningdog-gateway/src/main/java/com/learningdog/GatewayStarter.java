0f7822e4a0be703bc45e528608f0ae1949095b45
getjiajia <2690469445@qq.com>
2023-05-27 15:50:56 +0800

    添加网关工程

---
+++ learningdog-gateway/src/main/java/com/learningdog/GatewayStarter.java
@@ -0,0 +1,13 @@
+package com.learningdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayStarter.class, args);
+    }
+
+}
